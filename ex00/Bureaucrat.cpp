123b56efcfd285dca9553eb2bbc4ef3c04e94a9b
Nonoka <nonokapiano@icloud.com>
2024-10-13 15:55:19 +0900

    Implement Bureaucrat class with constructors, grade validation, and exception handling

--- ex00/Bureaucrat.cpp
+++ ex00/Bureaucrat.cpp
@@ -1,10 +1,71 @@
-//konnsutorakuta
-//desutorakuta
-//GradeTooHighException
-//gradetoolo
-//getname
-//getgrade
-//incurimennto
-//dekurimennto
+#include "Bureaucrat.hpp"
 
-//<<no override
+Bureaucrat::Bureaucrat(const std::string& name, int grade) \
+: name_(name), grade_(grade) {
+	std::cout << "Bureaucrat Default Constructor called for " << this->name_ << \
+	" with grade of " << this->grade_<< std::endl;
+	if (this->grade_ > 150)
+		throw Bureaucrat::GradeTooLowException();
+	else if (this->grade_ < 1)
+		throw Bureaucrat::GradeTooHighException();
+}
+Bureaucrat::~Bureaucrat()
+{
+	std::cout << "Bureaucrat Destructor for " << this->getName() << " called" << std::endl;
+}
+const char*	Bureaucrat::GradeTooHighException::what(void) const throw() {
+    return ("Grade too height");
+}
+const char*	Bureaucrat::GradeTooHighException::what(void) const throw() {
+    return ("Grade too low");
+}
+const std::string	Bureaucrat::getName(void)const
+{
+	return (this->name_);
+}
+int	Bureaucrat::getGrade(void)const
+{
+	return (this->grade_);
+}
+void	Bureaucrat::decrementGrade(void) {
+
+    std::cout << "<Bureaucrat> decrementGrade() called for " << this->name_ << std::endl;
+	this->grade_--;
+    if (this->grade_ < 1)
+        throw Bureaucrat::GradeTooLowException();
+
+
+}
+void	Bureaucrat::incrementGrade(void) {
+
+    std::cout << "<Bureaucrat> decrementGrade() called for " << this->name_ << std::endl;
+    this->grade_++;
+    if (this->grade_ > 150)
+        throw Bureaucrat::GradeTooLowException();
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &src): name_(src.getName()), grade_(src.getGrade())
+{
+	std::cout << "Bureaucrat Copy Constructor called to copy " << src.getName() <<
+	" into " << this->getName() << std::endl;
+}
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &src)
+{
+	std::cout << "Bureaucrat Assignation operator called" << std::endl;
+	if (this->grade_ > 150)
+		throw Bureaucrat::GradeTooLowException();
+	else if (this->grade_ < 1)
+		throw Bureaucrat::GradeTooHighException();
+	if (this != &src) {
+        // constメンバでない部分だけ代入
+		this->grade_ = src.grade_;
+    	// const_cast<std::string&>(this->name_) = src.name_;
+	}
+	return *this;
+}
+
+
+std::ostream&	operator<<(std::ostream& stream, const Bureaucrat& bureaucrat) {
+	stream << bureaucrat.getName() << ", bureaucrat grade " << bureaucrat.getGrade() << ".";
+	return (stream);
+}

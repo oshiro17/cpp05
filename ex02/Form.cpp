b252ed3525031841dfe5963da3b8457f0cd72e62
Nonoka <nonokapiano@icloud.com>
2024-10-21 19:34:18 +0900

    Refactor: Indented code for better readability in Form class implementation

--- ex02/Form.cpp
+++ ex02/Form.cpp
@@ -1,13 +1,9 @@
 #include "Form.hpp"
 
-Form::Form(const std::string& name, \
-    const int grade_required_to_sign, const int grade_required_to_exec) \
-    : grade_required_to_sign_(grade_required_to_sign), \
-      grade_required_to_exec_(grade_required_to_exec), \
+Form::Form(const std::string& name, const int grade_required_to_sign, const int grade_required_to_exec)
+    : grade_required_to_sign_(grade_required_to_sign), grade_required_to_exec_(grade_required_to_exec), 
       name_(name), signed_(false)
 {
-    std::cout << "Form  Default Constructor called for " << this->name_ << std::endl;
-
     if (this->grade_required_to_sign_ < 1 || this->grade_required_to_exec_ < 1) {
         throw GradeTooHighException();
     }
@@ -16,28 +12,23 @@ Form::Form(const std::string& name, \
     }
 }
 
-Form::Form(const Form& src) : grade_required_to_sign_(src.getGradeRequiredToSign()), \
-      grade_required_to_exec_(src.getGradeRequiredToExec()), \
-      name_(src.getName()), \
-      signed_(src.getSigned())
+Form::Form(const Form& src) 
+    : grade_required_to_sign_(src.getGradeRequiredToSign()), grade_required_to_exec_(src.getGradeRequiredToExec()), 
+      name_(src.getName()), signed_(src.getSigned())
 {
-    std::cout << "Form  Copy Constructor called for " << this->name_ << std::endl;
-
     if (this->grade_required_to_sign_ < 1 || this->grade_required_to_exec_ < 1) {
         throw Form::GradeTooHighException();
     }
     if (this->grade_required_to_sign_ > 150 || this->grade_required_to_exec_ > 150) {
         throw Form::GradeTooLowException();
     }
-
 }
 
-Form &Form::operator=(const Form& src)
+Form& Form::operator=(const Form& src)
 {
-    std::cout << "Form Assignation operator called" << std::endl;
-    if (src.getGradeRequiredToExec()<1 || src.getGradeRequiredToSign()<1)
+    if (src.getGradeRequiredToExec() < 1 || src.getGradeRequiredToSign() < 1)
         throw GradeTooHighException();
-    if (src.getGradeRequiredToExec()>150 || src.getGradeRequiredToSign()>150)
+    if (src.getGradeRequiredToExec() > 150 || src.getGradeRequiredToSign() > 150)
         throw GradeTooLowException();
     if (this != &src)
     {
@@ -46,43 +37,43 @@ Form &Form::operator=(const Form& src)
         const_cast<int&>(this->grade_required_to_exec_) = src.grade_required_to_exec_;
         const_cast<int&>(this->grade_required_to_sign_) = src.grade_required_to_sign_;
     }
-    return(*this);
-
+    return (*this);
 }
 
 Form::~Form()
 {
-	std::cout << "Form Destructor for " << this->getName() << " called" << std::endl;
 }
 
-const char*	Form::GradeTooHighException::what(void) const throw() {
+const char* Form::GradeTooHighException::what(void) const throw() {
     return ("Grade too high");
 }
-const char*	Form::GradeTooLowException::what(void) const throw() {
+
+const char* Form::GradeTooLowException::what(void) const throw() {
     return ("Grade too low");
 }
 
-const std::string&	Form::getName(void) const{
-    return(this->name_);
+const std::string& Form::getName(void) const {
+    return (this->name_);
 }
-bool    Form::getSigned(void) const{
-    return(this->signed_);
+
+bool Form::getSigned(void) const {
+    return (this->signed_);
 }
-int Form::getGradeRequiredToSign(void)const
-{
-    return(this->grade_required_to_sign_);
+
+int Form::getGradeRequiredToSign(void) const {
+    return (this->grade_required_to_sign_);
 }
-int Form::getGradeRequiredToExec(void)const{
-    return(this->grade_required_to_exec_);
+
+int Form::getGradeRequiredToExec(void) const {
+    return (this->grade_required_to_exec_);
 }
-void	Form::beSigned(const Bureaucrat& bc) {
-	std::cout << this \
-		<< "<Form> beSigned() called (" \
-		<< this->name_ << std::endl;
-	if (this->signed_== false &&this->grade_required_to_sign_< bc.getGrade())
-		throw Form::GradeTooLowException();
-	this->signed_ = true;
+
+void Form::beSigned(const Bureaucrat& bc) {
+    if (!this->signed_ && this->grade_required_to_sign_ < bc.getGrade())
+        throw Form::GradeTooLowException();
+    this->signed_ = true;
 }
+
 std::ostream& operator<<(std::ostream& os, const Form& form) {
     os << "Form: " << form.getName()
        << ", signed: " << (form.getSigned() ? "yes" : "no")
